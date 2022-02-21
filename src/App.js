84fb80872f44d7430d81e462edc50df6db670962
marvinscott <marvin.scott39@outlook.com>
2022-02-20 18:20:38 -0800

    Updated pages: Set up initial draft of website

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
-import CompetitionSchedules from "./pages/competitionSchedules/CompetitionSchedules";
+//import TournamentRegistration from "./components/tournamentRegistration/TournamentRegistration";
+//import CompetitionSchedules from "./pages/competitionSchedules/CompetitionSchedules";
+
 
     function App() {
 
-      return <CompetitionSchedules/>;
+      return <Profile/>;
     }
 
 
