c2f0b7561a44c4a5cceb4b5d7c9c436ad2882924
marvinscott <marvin.scott39@outlook.com>
2022-02-25 21:52:39 -0800

    Added a few images to users so UI doesn't look so bad.

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -37,8 +37,8 @@ export default function Post({ post }) {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img className="likeIcon" src="assets/like.png" onClick={likeHandler} alt="" />
-            <img className="likeIcon" src="assets/heart.png" onClick={likeHandler} alt="" />
+            <img className="likeIcon" src="assets/post/like.jpeg" onClick={likeHandler} alt="" />
+            <img className="likeIcon" src="assets/post/heart.jpeg" onClick={likeHandler} alt="" />
             <span className="postLikeCounter">{like} people like it</span>
           </div>
           <div className="postBottomRight">
